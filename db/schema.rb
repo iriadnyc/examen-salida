337387dbd96750d7c3356df5edb206323ffd7071
Miguel Olave <molavec@gmail.com>
2022-03-05 16:20:26 -0300

    users funcionando

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_175304) do
-
-  create_table "comments", force: :cascade do |t|
-    t.text "text"
-    t.date "date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2022_03_05_191913) do
 
   create_table "users", force: :cascade do |t|
     t.string "user"
