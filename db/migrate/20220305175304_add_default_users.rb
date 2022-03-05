63fd491610c59104cbb7cb248f27ee38bb01153f
Miguel Olave <molavec@gmail.com>
2022-03-05 15:09:01 -0300

    Añade usuarios exitosamente

--- db/migrate/20220305175304_add_default_users.rb
+++ db/migrate/20220305175304_add_default_users.rb
@@ -1,7 +1,7 @@
 class AddDefaultUsers < ActiveRecord::Migration[6.1]
   def up
-    Users.create(user: 'admin@gmail.com', pass: '291ABASD932')
-    Users.create(user: 'usuario1@gmail.com', pass: '291ABASD934')
-    Users.create(user: 'usuario2@gmail.com', pass: '281ABASD932')
+    User.create(user: 'admin@gmail.com', pass: '291ABASD932')
+    User.create(user: 'usuario1@gmail.com', pass: '291ABASD934')
+    User.create(user: 'usuario2@gmail.com', pass: '281ABASD932')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_165419) do
+ActiveRecord::Schema.define(version: 2022_03_05_175304) do
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
