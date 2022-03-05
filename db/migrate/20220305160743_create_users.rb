1b75c87d013827a1a289a11d1d4002493e9b4f40
Miguel Olave <molavec@gmail.com>
2022-03-05 13:53:17 -0300

    Migración ejecutada

--- db/migrate/20220305160743_create_users.rb
+++ db/migrate/20220305160743_create_users.rb
@@ -1,4 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
+  
   def change
     create_table :users do |t|
       t.string :user
@@ -6,5 +7,9 @@ class CreateUsers < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
+
+    SystemSetting.create user: 'admin@gmail.com', pass: '291ABASD932'
+    SystemSetting.create user: 'usuario1@gmail.com', pass: '291ABASD934'
+    SystemSetting.create user: 'usuario2@gmail.com', pass: '281ABASD932'
   end
 end
